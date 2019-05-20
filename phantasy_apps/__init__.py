496ba90390710559c9fcc7202d71d13fe7f9b0f6
Tong Zhang <zhangt@frib.msu.edu>
2019-05-20 15:35:45 -0400

    MNT: Bump up version to 2.0.1.

--- phantasy_apps/__init__.py
+++ phantasy_apps/__init__.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
-"""
-Applications
-"""
-
-# try:
-#     from phantasy_apps import *
-# except ImportError:
-#     print("'phantasy-apps' is required...")
-
-
+import logging
 import os
+
+logging.getLogger(__name__).setLevel(logging.INFO)
+logging.basicConfig(
+        format="%(levelname)s: %(asctime)s: %(name)s: %(message)s"
+)
+
+__authors__ = "Tong Zhang"
+__copyright__ = "(c) 2019, Facility for Rare Isotope beams," \
+                " Michigan State University"
+__contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
+__version__ = "2.0.1"
+
+__doc__ = \
+"""High-level physics controls applications built upon `PHANTASY`.
+
+:version: %s
+:authors: %s
+""" % (__version__, __authors__)
+
 
 k = "PHANTASY_CONFIG_DIR"
 os.environ.setdefault(k, "/usr/lib/phantasy-machines")
-
