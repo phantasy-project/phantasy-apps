26a69a84c9dfda8f0ace229674ed157b80ae0b96
Tong Zhang <zhangt@frib.msu.edu>
2019-06-06 09:35:50 -0400

    MNT/AS: Bump up version to 2.0.

--- phantasy_apps/allison_scanner/__init__.py
+++ phantasy_apps/allison_scanner/__init__.py
@@ -17,7 +17,7 @@ __copyright__ = "(c) 2019, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
 __title__ = "Allison Scanner: Beam Twiss Parameters Measurement"
-__version__ = '1.0'
+__version__ = '2.0'
 
 
 def run(cli=False):
