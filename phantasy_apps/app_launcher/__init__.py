264e54f69dbf0d261a0e885e73d58deb2ec536a9
Tong Zhang <zhangt@frib.msu.edu>
2019-07-09 20:47:06 -0400

    MNT/AL: Bump up version to 3.2.

--- phantasy_apps/app_launcher/__init__.py
+++ phantasy_apps/app_launcher/__init__.py
@@ -10,7 +10,7 @@ __copyright__ = "(c) 2019, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
 __title__ = "Global Launcher for FRIB Physics Apps"
-__version__ = '3.1'
+__version__ = '3.2'
 
 
 def run(cli=False):
