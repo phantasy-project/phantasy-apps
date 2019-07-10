34a628e88e6978653984060819f6fcdf6b00bd13
Tong Zhang <zhangt@frib.msu.edu>
2019-07-09 20:22:10 -0400

    MNT/ORM: Bump up version to 3.4.

--- phantasy_apps/orm/__init__.py
+++ phantasy_apps/orm/__init__.py
@@ -11,7 +11,7 @@ __copyright__ = "(c) 2018-2019, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
 __title__ = "Optics Response Matrix: Steer the Beam Trajectory Like A Master"
-__version__ = '3.3'
+__version__ = '3.4'
 
 
 def run(cli=False):
