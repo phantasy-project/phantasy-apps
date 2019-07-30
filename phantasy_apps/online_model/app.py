8bbb447f9db4b687f3696058882830c3b313ee9d
Tong Zhang <zhangt@frib.msu.edu>
2019-07-30 19:04:27 -0400

    ENH/OM: Implement physics modeling with FLAME.

---
+++ phantasy_apps/online_model/app.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Template Python module generated based on 'app_template', 'phantasy-ui'
+is required to make it executable as a PyQt5 app.
+"""
+
+from functools import partial
+import time
+from numpy import ndarray
+
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtCore import QVariant
+from PyQt5.QtWidgets import QMainWindow
+
+from phantasy_ui import BaseAppForm
+from phantasy_ui import get_open_filename
+from phantasy_ui.widgets import LatticeWidget
+from phantasy_ui.widgets import DataAcquisitionThread as DAQT
+
+from phantasy_apps.correlation_visualizer.utils import delayed_exec
+
+from .ui.ui_app import Ui_MainWindow
+
+
+class MyAppWindow(BaseAppForm, Ui_MainWindow):
+
+    stop_signal = pyqtSignal(bool)
+    traj_lineChanged = pyqtSignal(int)
+    traj_xdataChanged = pyqtSignal(QVariant)
+    traj_ydataChanged = pyqtSignal(QVariant)
+    env_lineChanged = pyqtSignal(int)
+    env_xdataChanged = pyqtSignal(QVariant)
+    env_ydataChanged = pyqtSignal(QVariant)
+    data_updated = pyqtSignal(ndarray, ndarray, ndarray, ndarray, ndarray)
+
+    def __init__(self, version, **kws):
+        super(self.__class__, self).__init__()
+
+        # app version, title
+        self.setAppVersion(version)
+        self.setAppTitle("Online Model")
+
+        # app info in about dialog
+        self.app_about_info = """
+            <html>
+            <h4>About Physics Online Model</h4>
+            <p>This app is created for the live modeling of accelerator.
+            </p>
+            <p>Copyright (c) 2019 Facility for Rare Isotope Beams and other contributors.</p>
+            </html>
+        """.format(self.getAppVersion())
+
+        # UI
+        self.setupUi(self)
+        self.postInitUi()
+
+        #
+        self._post_init()
+
+    def _post_init(self):
+        #
+        self.lattice_load_window = None
+        self._stop = False
+        self.stop_signal.connect(self.on_set_stop_signal)
+
+        # start/stop btns
+        self.start_btn.clicked.connect(self.on_start)
+        self.stop_btn.clicked.connect(self.on_stop)
+
+        # load latfile for reading initial condition
+        self.browse_btn.clicked.connect(self.on_open_latfile)
+
+        #
+        self._init_data_viz()
+
+    def _init_data_viz(self):
+        # init data viz
+        o1, o2 = self.trajectory_plot, self.envelope_plot
+        self.traj_lineChanged.connect(o1.setLineID)
+        self.traj_xdataChanged.connect(o1.setXData)
+        self.traj_ydataChanged.connect(o1.setYData)
+        self.env_lineChanged.connect(o2.setLineID)
+        self.env_xdataChanged.connect(o2.setXData)
+        self.env_ydataChanged.connect(o2.setYData)
+
+        self.data_updated.connect(self.on_data_updated)
+
+        self.trajectory_plot.add_curve()
+        self.envelope_plot.add_curve()
+
+        for s in ('traj', 'env'):
+            for i in range(2):
+                getattr(self, '{}_lineChanged'.format(s)).emit(i)
+                getattr(self, '{}_xdataChanged'.format(s)).emit([])
+                getattr(self, '{}_ydataChanged'.format(s)).emit([])
+
+
+    @pyqtSlot()
+    def on_start(self):
+        """Start modeling.
+        """
+        if self._stop:
+            self.set_widgets_status("STOP")
+
+            return
+
+        self._settling_sec = self.dtsec_dsbox.value()
+        self.mth = DAQT(daq_func=self.model_single, daq_seq=range(1))
+        self.mth.daqStarted.connect(partial(self.set_widgets_status, "START"))
+        self.mth.resultsReady.connect(self.on_results_ready)
+        self.mth.finished.connect(self.on_start)
+        self.mth.start()
+
+    def on_data_updated(self, s, x, y, rx, ry):
+        self.traj_lineChanged.emit(0)
+        self.traj_xdataChanged.emit(s)
+        self.traj_ydataChanged.emit(x)
+        self.traj_lineChanged.emit(1)
+        self.traj_xdataChanged.emit(s)
+        self.traj_ydataChanged.emit(y)
+
+        self.env_lineChanged.emit(0)
+        self.env_xdataChanged.emit(s)
+        self.env_ydataChanged.emit(rx)
+        self.env_lineChanged.emit(1)
+        self.env_xdataChanged.emit(s)
+        self.env_ydataChanged.emit(ry)
+
+    def model_single(self, iiter):
+        # model with live settings.
+        t0 = time.time()
+        print("-- Sync live settings...")
+        self._lat.sync_settings()
+        print("-- Run physics model...")
+        _, fm = self._lat.run()
+        r, s = fm.run(monitor='all')
+        data = fm.collect_data(r, pos=True, x0=True, y0=True,
+                               xrms=True, yrms=True)
+        s = data['pos']
+        traj_x, traj_y = data['x0'], data['y0']
+        env_x, env_y = data['xrms'], data['yrms']
+        self.data_updated.emit(s, traj_x, traj_y, env_x, env_y)
+        dt = self._settling_sec - (time.time() - t0)
+        if dt >= 0:
+            time.sleep(dt)
+
+    def on_results_ready(self, r):
+        print(r)
+
+    def set_widgets_status(self, status):
+        olist1 = (self.start_btn, )
+        olist2 = (self.stop_btn, )
+        if status != "START":
+            [o.setEnabled(True) for o in olist1]
+            [o.setEnabled(False) for o in olist2]
+        else:
+            [o.setEnabled(False) for o in olist1]
+            [o.setEnabled(True) for o in olist2]
+
+    @pyqtSlot()
+    def on_stop(self):
+        """Stop modeling.
+        """
+        self.stop_signal.emit(True)
+        delayed_exec(lambda:self.stop_signal.emit(False), 2000)
+
+    @pyqtSlot()
+    def on_open_latfile(self):
+        """Open latfile.
+        """
+        filepath, ext = get_open_filename(self,
+                type_filter="FLAME Lattice Files (*.lat)")
+        if filepath is None:
+            return
+        self.latpath_lineEdit.setText(filepath)
+
+    @pyqtSlot()
+    def onLoadLatticeAction(self):
+        """Load machine/segment configurations.
+        """
+        if self.lattice_load_window is None:
+            self.lattice_load_window = LatticeWidget()
+        self.lattice_load_window.show()
+        self.lattice_load_window.latticeChanged.connect(self.update_mp)
+
+    def update_mp(self, o):
+        self._mp = o
+        self._lat = o.work_lattice_conf
+        mach, segm = o.last_machine_name, o.last_lattice_name
+        self.machine_lineEdit.setText(mach)
+        self.segment_lineEdit.setText(segm)
+
+    @pyqtSlot(bool)
+    def on_set_stop_signal(self, f):
+        self._stop = f
+
+
+
+if __name__ == "__main__":
+    from PyQt5.QtWidgets import QApplication
+    import sys
+
+    version = 0.1
+    app = QApplication(sys.argv)
+    w = MyAppWindow(version)
+    w.show()
+    w.setWindowTitle("This is an app from template")
+    sys.exit(app.exec_())
