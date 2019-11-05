480c5a7fe8a71119a3bff33541383528bbb7ccef
Tong Zhang <zhangt@frib.msu.edu>
2019-11-05 11:27:00 -0500

    ENH/AS: Add columns for total points and shape for scan settings list.

--- phantasy_apps/allison_scanner/utils.py
+++ phantasy_apps/allison_scanner/utils.py
@@ -90,14 +90,17 @@ class SettingsModel(QStandardItemModel):
 
         # header
         self.header = self.h_idx, self.h_name, self.h_xoy, \
+                      self.h_npoints, self.h_shape, \
                       self.h_pos_begin, self.h_pos_end, self.h_pos_step, \
                       self.h_volt_begin, self.h_volt_end, self.h_volt_step, \
                       self.h_apply, self.h_delete = \
             "", "Name", "X/Y", \
+            "Points", "Shape", \
             "Pos Begin", "Pos End", "Pos Step", \
             "Volt Begin", "Volt End", "Volt Step", \
             "Apply", "Delete"
         self.ids = self.i_idx, self.i_name, self.i_xoy, \
+                   self.i_npoints, self.i_shape, \
                    self.i_pos_begin, self.i_pos_end, self.i_pos_step, \
                    self.i_volt_begin, self.i_volt_end, self.i_volt_step, \
                    self.i_apply, self.i_delete = \
@@ -129,6 +132,11 @@ class SettingsModel(QStandardItemModel):
             item_name = QStandardItem(name)
             item_xoy = QStandardItem(xoy)
 
+            nx = int((pe - pb) / ps + 1)
+            ny = int((ve - vb) / vs + 1)
+            item_nsize = QStandardItem('{0:d}'.format(nx * ny))
+            item_shape = QStandardItem('{0:d}{2}{1:d}'.format(ny, nx, u"\u00D7"))
+
             item_pb = QStandardItem(self.fmt.format(pb))
             item_pe = QStandardItem(self.fmt.format(pe))
             item_ps = QStandardItem(self.fmt.format(ps))
@@ -137,10 +145,11 @@ class SettingsModel(QStandardItemModel):
             item_ve = QStandardItem(self.fmt.format(ve))
             item_vs = QStandardItem(self.fmt.format(vs))
 
-            item_apply = QStandardItem('Apply')
-            item_delete = QStandardItem('Delete')
+            item_apply = QStandardItem('')
+            item_delete = QStandardItem('')
 
             for item in (item_idx, item_name, item_xoy,
+                         item_nsize, item_shape,
                          item_pb, item_pe, item_ps,
                          item_vb, item_ve, item_vs,
                          item_apply, item_delete,):
@@ -155,6 +164,7 @@ class SettingsModel(QStandardItemModel):
         v = self._v
         for i, d in enumerate(self._data):
             btn1 = QToolButton()
+            btn1.setAutoRaise(True)
             btn1.setIcon(QIcon(QPixmap(":/icons/apply.png")))
             btn1.setToolTip("Click to apply current settings.")
             idx1 = self.index(i, self.i_apply)
@@ -162,6 +172,7 @@ class SettingsModel(QStandardItemModel):
             btn1.clicked.connect(partial(self.on_apply, d['timestamp']))
 
             btn2 = QToolButton()
+            btn2.setAutoRaise(True)
             btn2.setIcon(QIcon(QPixmap(":/icons/delete-as.png")))
             btn2.setToolTip("Click to remove current settings.")
             idx2 = self.index(i, self.i_delete)
