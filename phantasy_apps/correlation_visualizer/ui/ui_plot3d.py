1ecff805489b6b190342c357b6773a9ea15b8935
Tong Zhang <zhangt@frib.msu.edu>
2019-11-22 15:23:40 -0500

    UI/CV: Create new UI for 3D dataviz and update 2D scan window.

---
+++ phantasy_apps/correlation_visualizer/ui/ui_plot3d.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui_plot3d.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1280, 1024)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout_3 = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.splitter = QtWidgets.QSplitter(self.centralwidget)
+        self.splitter.setOrientation(QtCore.Qt.Horizontal)
+        self.splitter.setObjectName("splitter")
+        self.groupBox = QtWidgets.QGroupBox(self.splitter)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+                                           QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(2)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.groupBox.sizePolicy().hasHeightForWidth())
+        self.groupBox.setSizePolicy(sizePolicy)
+        self.groupBox.setObjectName("groupBox")
+        self.gridLayout = QtWidgets.QGridLayout(self.groupBox)
+        self.gridLayout.setContentsMargins(8, 8, 8, 8)
+        self.gridLayout.setObjectName("gridLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.matplotlibimageWidget = MatplotlibImageWidget(self.groupBox)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding,
+                                           QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.matplotlibimageWidget.sizePolicy().hasHeightForWidth())
+        self.matplotlibimageWidget.setSizePolicy(sizePolicy)
+        self.matplotlibimageWidget.setFigureXlabel("")
+        font = QtGui.QFont()
+        font.setFamily("aakar")
+        font.setPointSize(14)
+        self.matplotlibimageWidget.setFigureXYlabelFont(font)
+        self.matplotlibimageWidget.setProperty("figureToolbarToggle", False)
+        self.matplotlibimageWidget.setColorBarToggle(True)
+        self.matplotlibimageWidget.setAutoColorLimit(True)
+        self.matplotlibimageWidget.setObjectName("matplotlibimageWidget")
+        self.verticalLayout.addWidget(self.matplotlibimageWidget)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.norm_chkbox = QtWidgets.QCheckBox(self.groupBox)
+        self.norm_chkbox.setObjectName("norm_chkbox")
+        self.horizontalLayout.addWidget(self.norm_chkbox)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.splitter, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.groupBox.setTitle(_translate("MainWindow", "3D Plot"))
+        self.matplotlibimageWidget.setFigureAspectRatio(
+            _translate("MainWindow", "auto"))
+        self.matplotlibimageWidget.setColorMap(_translate("MainWindow", "jet"))
+        self.norm_chkbox.setText(
+            _translate("MainWindow", "Normalize Intensity"))
+
+
+from mpl4qt.widgets.mplimagewidget import MatplotlibImageWidget
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
