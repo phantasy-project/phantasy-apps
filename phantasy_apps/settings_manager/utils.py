4f0fd17f111549f53076f41eb4448c23e0209b28
Tong Zhang <zhangt@frib.msu.edu>
2019-11-27 12:13:23 -0500

    ENH/SM: Update SettingsModel to support PV values updating.

--- phantasy_apps/settings_manager/utils.py
+++ phantasy_apps/settings_manager/utils.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from collections import OrderedDict
+from functools import partial
 
 from PyQt5.QtCore import Qt
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtCore import QVariant
 from PyQt5.QtGui import QStandardItem
 from PyQt5.QtGui import QStandardItemModel
 from PyQt5.QtWidgets import QAbstractScrollArea
@@ -13,7 +16,9 @@ try:
 except NameError:
     basestring = str
 
-COLUMN_NAMES1 = ['Device', 'Field', 'Setting']
+FMT = "{0:.6g}"
+
+COLUMN_NAMES1 = ['Device', 'Field', 'Setpoint', 'Live Readback', 'Live Setpoint']
 COLUMN_SFIELD_MAP = OrderedDict((
     ('Type', 'family'),
     ('Pos [m]', 'sb'),
@@ -34,20 +39,25 @@ class SettingsModel(QStandardItemModel):
     flat_settings : list
         List of setting with the format of ``(elem, fname, fval0)``,
         ``elem`` is CaElement object, ``fname`` is field name, ``fval0`` is
-        saved field value.
+        saved field value of setpoint.
     """
+
+    item_changed = pyqtSignal(QVariant)
+
     def __init__(self, parent, flat_settings):
         super(self.__class__, self).__init__(parent)
         self._settings = flat_settings
         self._tv = parent
+        #
+        self._pvs = []
 
         # header
-        self.header = self.h_name, self.h_field, self.h_val0, \
-                self.h_type, self.h_pos, self.h_len, self.h_index \
-                = COLUMN_NAMES
-        self.ids = self.i_name, self.i_field, self.i_val0, \
-                self.i_type, self.i_pos, self.i_len, self.i_index \
-                = range(len(self.header))
+        self.header = self.h_name, self.h_field, self.h_val0, self.h_rd, self.h_cset, \
+                      self.h_type, self.h_pos, self.h_len, self.h_index \
+            = COLUMN_NAMES
+        self.ids = self.i_name, self.i_field, self.i_val0, self.i_rd, self.i_cset, \
+                   self.i_type, self.i_pos, self.i_len, self.i_index \
+            = range(len(self.header))
 
         # set data (pure)
         self.set_data()
@@ -56,12 +66,30 @@ class SettingsModel(QStandardItemModel):
         for i, s in zip(self.ids, self.header):
             self.setHeaderData(i, Qt.Horizontal, s)
 
+        #
+        self.item_changed.connect(self.update_item)
+
+    def update_item(self, p):
+        self.setItem(*p)
+
     def set_data(self):
         for elem, fname, fval0 in self._settings:
-            item0 = QStandardItem(elem.name)
-            item1 = QStandardItem(fname)
-            item2 = QStandardItem('{:.4f}'.format(fval0))
-            row = [item0, item1, item2]
+            item_ename = QStandardItem(elem.name)
+
+            print('{}[{}]'.format(elem.name, fname))
+            fld = elem.get_field(fname)
+            item_ename.fobj = fld
+            if fld is None:
+                print("{} [{}] is invalid.".format(elem.name, fname))
+                continue
+
+            item_fname = QStandardItem(fname)
+            item_val0 = QStandardItem(FMT.format(fval0))
+            item_rd = QStandardItem(FMT.format(fld.value))
+            # item_rd = QStandardItem('Current Readback')
+            item_cset = QStandardItem(FMT.format(elem.current_setting(fname)))
+            # item_cset = QStandardItem('Current Setpoint')
+            row = [item_ename, item_fname, item_val0, item_rd, item_cset, ]
             for i, f in enumerate(COLUMN_NAMES):
                 if f in COLUMN_NAMES_ATTR:
                     v = getattr(elem, COLUMN_SFIELD_MAP[f])
@@ -76,18 +104,41 @@ class SettingsModel(QStandardItemModel):
     def set_model(self):
         # set model, set field column
         self._tv.setModel(self)
-        self.__post_init_ui()
+        #
+        self.set_cbs()
+        self.__post_init_ui(self._tv)
 
-    def __post_init_ui(self):
+    def set_cbs(self):
+        def _cb(row, col, fld, vtyp, **kws):
+            if vtyp == 'rd':
+                val = fld.value
+            else:
+                val = fld.current_setting()
+            item = QStandardItem(FMT.format(val))
+            self.item_changed.emit((row, col, item))
+
+        for irow in range(self.rowCount()):
+            item0 = self.item(irow, self.i_name)
+            fld = item0.fobj
+            if fld is None:
+                continue
+            rd_pv0 = fld.readback_pv[0]
+            sp_pv0 = fld.setpoint_pv[0]
+            for (icol, pv, vtyp) in zip(
+                    (self.i_rd, self.i_cset),
+                    (rd_pv0, sp_pv0),
+                    ('rd', 'sp')):
+                pv.add_callback(partial(_cb, irow, icol, fld, vtyp))
+                self._pvs.append(pv)
+
+    def __post_init_ui(self, tv):
         # view properties
-        tv = self._tv
         tv.setStyleSheet("font-family: monospace;")
         tv.setAlternatingRowColors(True)
+        for i in self.ids:
+            tv.resizeColumnToContents(i)
         tv.setSortingEnabled(True)
-        tv.resizeColumnToContents(self.i_name)
         self.sort(self.i_index, Qt.AscendingOrder)
         tv.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
-        #tv.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        #tv.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-
-
+        # tv.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        # tv.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
