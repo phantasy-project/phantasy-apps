13be573295f633f49babaa9d5b10d4365f6b4533
Tong Zhang <zhangt@frib.msu.edu>
2019-11-27 12:26:41 -0500

    UI/SM: Update UIs.

--- phantasy_apps/settings_manager/ui/ui_app.py
+++ phantasy_apps/settings_manager/ui/ui_app.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui_app.ui'
 #
-# Created by: PyQt5 UI code generator 5.5.1
+# Created by: PyQt5 UI code generator 5.10.1
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,110 +12,90 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1920, 1440)
-        icon = QtGui.QIcon()
-        icon.addPixmap(
-            QtGui.QPixmap(":/icons/sm.png"), QtGui.QIcon.Normal,
-            QtGui.QIcon.Off)
-        MainWindow.setWindowIcon(icon)
+        MainWindow.resize(1600, 1200)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName("gridLayout")
         self.treeView = QtWidgets.QTreeView(self.centralwidget)
-        self.treeView.setSizeAdjustPolicy(
-            QtWidgets.QAbstractScrollArea.AdjustIgnored)
         self.treeView.setObjectName("treeView")
-        self.gridLayout.addWidget(self.treeView, 0, 0, 1, 1)
-        self.horizontalLayout = QtWidgets.QHBoxLayout()
-        self.horizontalLayout.setContentsMargins(-1, 0, -1, -1)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        spacerItem = QtWidgets.QSpacerItem(40, 20,
+        self.gridLayout.addWidget(self.treeView, 0, 0, 1, 2)
+        spacerItem = QtWidgets.QSpacerItem(1482, 20,
                                            QtWidgets.QSizePolicy.Expanding,
                                            QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem)
+        self.gridLayout.addItem(spacerItem, 1, 0, 1, 1)
         self.apply_btn = QtWidgets.QPushButton(self.centralwidget)
         self.apply_btn.setObjectName("apply_btn")
-        self.horizontalLayout.addWidget(self.apply_btn)
-        self.gridLayout.addLayout(self.horizontalLayout, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.apply_btn, 1, 1, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1920, 30))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1600, 31))
         self.menubar.setObjectName("menubar")
         self.menu_File = QtWidgets.QMenu(self.menubar)
         self.menu_File.setObjectName("menu_File")
-        self.menuEdit = QtWidgets.QMenu(self.menubar)
-        self.menuEdit.setObjectName("menuEdit")
         self.menu_Help = QtWidgets.QMenu(self.menubar)
         self.menu_Help.setObjectName("menu_Help")
         MainWindow.setMenuBar(self.menubar)
-        self.actionSave = QtWidgets.QAction(MainWindow)
-        icon1 = QtGui.QIcon()
-        icon1.addPixmap(
-            QtGui.QPixmap(":/icons/save.png"), QtGui.QIcon.Normal,
-            QtGui.QIcon.Off)
-        self.actionSave.setIcon(icon1)
-        self.actionSave.setObjectName("actionSave")
-        self.action_load_from_snp = QtWidgets.QAction(MainWindow)
-        icon2 = QtGui.QIcon()
-        icon2.addPixmap(
-            QtGui.QPixmap(":/icons/load.png"), QtGui.QIcon.Normal,
-            QtGui.QIcon.Off)
-        self.action_load_from_snp.setIcon(icon2)
-        self.action_load_from_snp.setObjectName("action_load_from_snp")
+        self.toolBar = QtWidgets.QToolBar(MainWindow)
+        self.toolBar.setObjectName("toolBar")
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+        self.action_About = QtWidgets.QAction(MainWindow)
+        self.action_About.setObjectName("action_About")
+        self.actionAbout_Qt = QtWidgets.QAction(MainWindow)
+        self.actionAbout_Qt.setObjectName("actionAbout_Qt")
         self.actionE_xit = QtWidgets.QAction(MainWindow)
         self.actionE_xit.setObjectName("actionE_xit")
         self.actionContents = QtWidgets.QAction(MainWindow)
         self.actionContents.setObjectName("actionContents")
-        self.actionAbout = QtWidgets.QAction(MainWindow)
-        self.actionAbout.setObjectName("actionAbout")
-        self.actionAbout_Qt = QtWidgets.QAction(MainWindow)
-        self.actionAbout_Qt.setObjectName("actionAbout_Qt")
-        self.menu_File.addAction(self.actionSave)
-        self.menu_File.addAction(self.action_load_from_snp)
-        self.menu_File.addSeparator()
+        self.action_Save = QtWidgets.QAction(MainWindow)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/sm-icons/save.png"),
+                       QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.action_Save.setIcon(icon)
+        self.action_Save.setObjectName("action_Save")
+        self.actionLoad_From_Snapshot = QtWidgets.QAction(MainWindow)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap(":/sm-icons/open.png"),
+                        QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionLoad_From_Snapshot.setIcon(icon1)
+        self.actionLoad_From_Snapshot.setObjectName("actionLoad_From_Snapshot")
         self.menu_File.addAction(self.actionE_xit)
         self.menu_Help.addAction(self.actionContents)
-        self.menu_Help.addAction(self.actionAbout)
+        self.menu_Help.addSeparator()
+        self.menu_Help.addAction(self.action_About)
         self.menu_Help.addAction(self.actionAbout_Qt)
         self.menubar.addAction(self.menu_File.menuAction())
-        self.menubar.addAction(self.menuEdit.menuAction())
         self.menubar.addAction(self.menu_Help.menuAction())
+        self.toolBar.addAction(self.action_Save)
+        self.toolBar.addAction(self.actionLoad_From_Snapshot)
 
         self.retranslateUi(MainWindow)
-        self.actionSave.triggered.connect(MainWindow.on_save)
-        self.action_load_from_snp.triggered.connect(
-            MainWindow.on_load_from_snp)
-        self.apply_btn.clicked.connect(MainWindow.on_apply_settings)
-        self.actionE_xit.triggered.connect(MainWindow.close)
-        self.actionAbout.triggered.connect(MainWindow.onAbout)
         self.actionAbout_Qt.triggered.connect(MainWindow.onAboutQt)
+        self.actionE_xit.triggered.connect(MainWindow.close)
+        self.action_About.triggered.connect(MainWindow.onAbout)
+        self.apply_btn.clicked.connect(MainWindow.on_apply_settings)
+        self.actionLoad_From_Snapshot.triggered.connect(
+            MainWindow.on_load_from_snp)
+        self.action_Save.triggered.connect(MainWindow.on_save)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.apply_btn.setToolTip(
-            _translate(
-                "MainWindow",
-                "<html><head/><body><p>Apply settings to accelerator</p></body></html>"
-            ))
         self.apply_btn.setText(_translate("MainWindow", "Apply"))
         self.menu_File.setTitle(_translate("MainWindow", "&File"))
-        self.menuEdit.setTitle(_translate("MainWindow", "Tools"))
         self.menu_Help.setTitle(_translate("MainWindow", "&Help"))
-        self.actionSave.setText(_translate("MainWindow", "Save"))
-        self.action_load_from_snp.setText(
-            _translate("MainWindow", "Load From Snapshot"))
-        self.action_load_from_snp.setShortcut(
-            _translate("MainWindow", "Ctrl+Shift+L"))
+        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
+        self.action_About.setText(_translate("MainWindow", "&About"))
+        self.action_About.setShortcut(_translate("MainWindow", "Ctrl+A"))
+        self.actionAbout_Qt.setText(_translate("MainWindow", "About Qt"))
         self.actionE_xit.setText(_translate("MainWindow", "E&xit"))
         self.actionE_xit.setShortcut(_translate("MainWindow", "Ctrl+W"))
         self.actionContents.setText(_translate("MainWindow", "Contents"))
         self.actionContents.setShortcut(_translate("MainWindow", "F1"))
-        self.actionAbout.setText(_translate("MainWindow", "About"))
-        self.actionAbout.setShortcut(_translate("MainWindow", "Ctrl+A"))
-        self.actionAbout_Qt.setText(_translate("MainWindow", "About Qt"))
+        self.action_Save.setText(_translate("MainWindow", "&Save"))
+        self.actionLoad_From_Snapshot.setText(
+            _translate("MainWindow", "Load From Snapshot"))
 
 
 from . import resources_rc
