88e96082de4c5997cee781e025625d38b147963e
Tong Zhang <zhangt@frib.msu.edu>
2020-02-20 10:54:38 -0500

    UI/SN: Update UI and resources.

--- phantasy_apps/synoptic_app/ui/ui_app.py
+++ phantasy_apps/synoptic_app/ui/ui_app.py
@@ -16,10 +16,34 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setContentsMargins(6, 12, 6, 6)
+        self.gridLayout.setSpacing(6)
         self.gridLayout.setObjectName("gridLayout")
         self.vbox = QtWidgets.QVBoxLayout()
+        self.vbox.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
         self.vbox.setObjectName("vbox")
-        self.gridLayout.addLayout(self.vbox, 0, 0, 1, 1)
+        self.gridLayout.addLayout(self.vbox, 1, 0, 1, 1)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+                                           QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.current_pointed_device_lineEdit = QtWidgets.QLineEdit(
+            self.centralwidget)
+        font = QtGui.QFont()
+        font.setFamily("Monospace")
+        self.current_pointed_device_lineEdit.setFont(font)
+        self.current_pointed_device_lineEdit.setObjectName(
+            "current_pointed_device_lineEdit")
+        self.horizontalLayout.addWidget(self.current_pointed_device_lineEdit)
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1600, 31))
@@ -29,6 +53,9 @@ class Ui_MainWindow(object):
         self.menu_Help = QtWidgets.QMenu(self.menubar)
         self.menu_Help.setObjectName("menu_Help")
         MainWindow.setMenuBar(self.menubar)
+        self.toolBar = QtWidgets.QToolBar(MainWindow)
+        self.toolBar.setObjectName("toolBar")
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
         self.action_About = QtWidgets.QAction(MainWindow)
         self.action_About.setObjectName("action_About")
         self.actionAbout_Qt = QtWidgets.QAction(MainWindow)
@@ -39,6 +66,24 @@ class Ui_MainWindow(object):
         self.actionContents.setObjectName("actionContents")
         self.action_Open = QtWidgets.QAction(MainWindow)
         self.action_Open.setObjectName("action_Open")
+        self.action_zoom_in = QtWidgets.QAction(MainWindow)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/sn-app/icons/zoom_in.png"),
+                       QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.action_zoom_in.setIcon(icon)
+        self.action_zoom_in.setObjectName("action_zoom_in")
+        self.action_zoom_out = QtWidgets.QAction(MainWindow)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap(":/sn-app/icons/zoom_out.png"),
+                        QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.action_zoom_out.setIcon(icon1)
+        self.action_zoom_out.setObjectName("action_zoom_out")
+        self.action_zoom_reset = QtWidgets.QAction(MainWindow)
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap(":/sn-app/icons/zoom_reset.png"),
+                        QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.action_zoom_reset.setIcon(icon2)
+        self.action_zoom_reset.setObjectName("action_zoom_reset")
         self.menu_File.addAction(self.action_Open)
         self.menu_File.addSeparator()
         self.menu_File.addAction(self.actionE_xit)
@@ -48,19 +93,27 @@ class Ui_MainWindow(object):
         self.menu_Help.addAction(self.actionAbout_Qt)
         self.menubar.addAction(self.menu_File.menuAction())
         self.menubar.addAction(self.menu_Help.menuAction())
+        self.toolBar.addAction(self.action_zoom_in)
+        self.toolBar.addAction(self.action_zoom_out)
+        self.toolBar.addAction(self.action_zoom_reset)
 
         self.retranslateUi(MainWindow)
         self.actionAbout_Qt.triggered.connect(MainWindow.onAboutQt)
         self.actionE_xit.triggered.connect(MainWindow.close)
         self.action_About.triggered.connect(MainWindow.onAbout)
         self.action_Open.triggered.connect(MainWindow.on_open_file)
+        self.action_zoom_in.triggered.connect(MainWindow.on_zoom_in_view)
+        self.action_zoom_out.triggered.connect(MainWindow.on_zoom_out_view)
+        self.action_zoom_reset.triggered.connect(MainWindow.on_zoom_reset_view)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Current Pointing Device"))
         self.menu_File.setTitle(_translate("MainWindow", "&File"))
         self.menu_Help.setTitle(_translate("MainWindow", "&Help"))
+        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
         self.action_About.setText(_translate("MainWindow", "&About"))
         self.action_About.setShortcut(_translate("MainWindow", "Ctrl+A"))
         self.actionAbout_Qt.setText(_translate("MainWindow", "About Qt"))
@@ -69,7 +122,12 @@ class Ui_MainWindow(object):
         self.actionContents.setText(_translate("MainWindow", "Contents"))
         self.actionContents.setShortcut(_translate("MainWindow", "F1"))
         self.action_Open.setText(_translate("MainWindow", "&Open"))
+        self.action_zoom_in.setText(_translate("MainWindow", "zoom_in"))
+        self.action_zoom_out.setText(_translate("MainWindow", "zoom_out"))
+        self.action_zoom_reset.setText(_translate("MainWindow", "zoom_reset"))
 
+
+from . import resources_rc
 
 if __name__ == "__main__":
     import sys
