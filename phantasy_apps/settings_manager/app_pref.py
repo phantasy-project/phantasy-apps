b65c982ac683541e921dc82fbcb10ea0852171b8
Tong Zhang <zhangt@frib.msu.edu>
2020-02-24 12:08:11 -0500

    REF/SM: Turn off initial settings in Preferences.

--- phantasy_apps/settings_manager/app_pref.py
+++ phantasy_apps/settings_manager/app_pref.py
@@ -8,7 +8,7 @@ from PyQt5.QtWidgets import QDialog
 from .ui.ui_preferences import Ui_Dialog
 
 DEFAULT_FIELD_INIT_MODE = 'model'
-DEFAULT_INIT_SETTINGS = True
+DEFAULT_INIT_SETTINGS = False
 DEFAULT_T_WAIT = 0.05
 DEFAULT_TOLERANCE = 0.10
 DEFAULT_CONFIG_SYNC_TIME_INTERVAL = 10  # second
