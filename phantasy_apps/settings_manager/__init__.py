34c3c75ea97611afce430a1a3834b490572312b1
Tong Zhang <zhangt@frib.msu.edu>
2020-03-24 16:37:42 -0400

    MNT/SM: Bump up version to 2.0.

--- phantasy_apps/settings_manager/__init__.py
+++ phantasy_apps/settings_manager/__init__.py
@@ -7,10 +7,10 @@ from phantasy_ui import QApp as QApplication
 from .app import SettingsManagerWindow
 
 __authors__ = "Tong Zhang"
-__copyright__ = "(c) 2019, Facility for Rare Isotope beams," \
+__copyright__ = "(c) 2019-2020, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
-__version__ = '1.1'
+__version__ = '2.0'
 
 
 def run(cli=False):
