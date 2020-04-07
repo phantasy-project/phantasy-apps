99f4da261f6dc5294c90f1913d0f32d7632bbd7e
Tong Zhang <zhangt@frib.msu.edu>
2020-04-07 16:52:47 -0400

    MNT: Migrate uptime() to phantasy_ui.templates.

--- phantasy_apps/utils.py
+++ phantasy_apps/utils.py
@@ -8,32 +8,6 @@ from mpl4qt.widgets.utils import MatplotlibCurveWidgetSettings
 TS_FMT = "%Y-%m-%dT%H:%M:%S.%f"
 
 
-def uptime(t):
-    """Convert *t* in second to uptime with the tuple of days, hours,
-    minutes, and seconds, and return string of uptime.
-
-    Examples
-    --------
-    >>> assert uptime(12345) == "03:25:45"
-    >>> assert uptime(123455) == "1 day, 10:17:35"
-    >>> assert uptime(1234555) == "14 days, 06:55:55"
-    """
-    MINUTE = 60
-    HOUR = MINUTE * 60
-    DAY = HOUR * 24
-
-    days = int(t / DAY)
-    hours = int((t % DAY) / HOUR)
-    minutes = int((t % HOUR) / MINUTE)
-    seconds = int(t % MINUTE)
-
-    s = ''
-    if days > 0:
-        s += str(days) + " " + (days == 1 and "day" or "days") + ", "
-    s += '{:02d}:{:02d}:{:02d}'.format(hours, minutes, seconds)
-    return s
-
-
 def find_dconf(app_name, conf_file):
     """Find app configuration file, searching the following locations:
     * ~/.phantasy/<conf_file>
