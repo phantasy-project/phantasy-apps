e70b5bc0a354d4d11c638c766c7073e84084d259
Tong Zhang <zhangt@frib.msu.edu>
2020-04-08 16:02:14 -0400

    ENH/VA: Add support to add new tab page as the control for new VA.

--- phantasy_apps/va/app.py
+++ phantasy_apps/va/app.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from functools import partial
+
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtCore import pyqtSlot
+
+from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QWidget
+
 from phantasy_ui import BaseAppForm
+from phantasy_ui.widgets import VirtualAcceleratorWidget
 
 from .ui.ui_app import Ui_MainWindow
 
@@ -36,3 +43,32 @@ class VALauncherWindow(BaseAppForm, Ui_MainWindow):
         # UI
         self.setupUi(self)
         self.postInitUi()
+
+        #
+        self._post_init()
+
+    def _post_init(self):
+        # initialize the first tab page
+        self.va_tab.currentChanged.emit(self.va_tab.count() - 1)
+
+    @pyqtSlot(int)
+    def on_current_tab_changed(self, index):
+        """Current tab is changed.
+        """
+        if index == self.va_tab.count() - 1:
+            w = self.create_new_va_page()
+            self.va_tab.insertTab(index, w, "New VA")
+            self.va_tab.setCurrentIndex(index)
+            va_page = w.findChild(VirtualAcceleratorWidget)
+            va_page.sig_va_name_changed.connect(
+                    partial(self.va_tab.setTabText, index))
+            va_page.reinit_va_info()
+
+    def create_new_va_page(self):
+        w = QWidget()
+        vbox = QVBoxLayout()
+        vbox.setContentsMargins(0, 0, 0, 0)
+        vbox.addWidget(VirtualAcceleratorWidget(w))
+        vbox.addStretch(1)
+        w.setLayout(vbox)
+        return w
