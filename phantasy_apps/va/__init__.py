f2ae83f93163d34b67258771eeda657a695b3a18
Tong Zhang <zhangt@frib.msu.edu>
2020-04-09 16:30:58 -0400

    MNT/VA: Bump up version to 2.1.

--- phantasy_apps/va/__init__.py
+++ phantasy_apps/va/__init__.py
@@ -7,10 +7,10 @@ from phantasy_ui import QApp as QApplication
 from .app import VALauncherWindow
 
 __authors__ = "Tong Zhang"
-__copyright__ = "(c) 2018, Facility for Rare Isotope beams," \
+__copyright__ = "(c) 2018-2020, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
-__version__ = '2.0'
+__version__ = '2.1'
 
 
 def run(cli=False):
