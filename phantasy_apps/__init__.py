460bc91d8788f1aae2b7901e6ad75fc1b922add7
Tong Zhang <zhangt@frib.msu.edu>
2020-04-21 17:17:46 -0400

    MNT: Bump up version to 3.0.1.

--- phantasy_apps/__init__.py
+++ phantasy_apps/__init__.py
@@ -11,7 +11,7 @@ __authors__ = "Tong Zhang"
 __copyright__ = "(c) 2019-2020, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 
 __doc__ = \
 """High-level physics controls applications built upon `PHANTASY`.
