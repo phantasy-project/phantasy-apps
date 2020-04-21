f670be7d6c6e3eb628db7977f8cd9a7e75b3efb4
Tong Zhang <zhangt@frib.msu.edu>
2020-04-21 09:33:09 -0400

    MNT/UN: Bump up version to 1.4.

--- phantasy_apps/unicorn_app/__init__.py
+++ phantasy_apps/unicorn_app/__init__.py
@@ -10,13 +10,15 @@ __authors__ = "Tong Zhang"
 __copyright__ = "(c) 2018, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
-__version__ = '1.3'
+__title__ = "Unicorn App: Manage Device Scaling Laws of Physics and Engineering Units"
+__version__ = '1.4'
 
 
 def run(cli=False):
     app = QApplication(sys.argv)
     w = UnicornApp(version=__version__)
     w.show()
+    w.setWindowTitle(__title__)
     if cli:
         app.exec_()
     else:
