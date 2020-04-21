dd8dbdbf73aa53cf87278fbda372fdc1d4d84c78
Tong Zhang <zhangt@frib.msu.edu>
2020-04-21 09:35:53 -0400

    MNT/LV: Bump up version to 1.3.

--- phantasy_apps/lattice_viewer/__init__.py
+++ phantasy_apps/lattice_viewer/__init__.py
@@ -11,7 +11,7 @@ __copyright__ = "(c) 2018, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
 __title__ = "Lattice Viewer: Investigate Loaded Lattice"
-__version__ = '1.2'
+__version__ = '1.3'
 
 
 def run(cli=False):
