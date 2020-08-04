960890c1ae7eb82da3137f0d8b97982e0b2d0bf4
Tong Zhang <zhangt@frib.msu.edu>
2020-08-04 16:31:45 -0400

    UI/AL: Update UI and resources.

--- phantasy_apps/app_launcher/ui/ui_app_info.py
+++ phantasy_apps/app_launcher/ui/ui_app_info.py
@@ -12,12 +12,109 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_InfoForm(object):
     def setupUi(self, InfoForm):
         InfoForm.setObjectName("InfoForm")
-        InfoForm.resize(968, 260)
+        InfoForm.resize(979, 258)
         InfoForm.setStyleSheet("QWidget#InfoForm {\n"
                                "    border: 2px solid #04B13B;\n"
                                "}")
         self.gridLayout_2 = QtWidgets.QGridLayout(InfoForm)
+        self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
         self.gridLayout_2.setObjectName("gridLayout_2")
+        self.label_3 = QtWidgets.QLabel(InfoForm)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+                                           QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.label_3.sizePolicy().hasHeightForWidth())
+        self.label_3.setSizePolicy(sizePolicy)
+        self.label_3.setStyleSheet("QLabel {\n"
+                                   "    margin-left: 10px;\n"
+                                   "    font-size: 14px;\n"
+                                   "}")
+        self.label_3.setObjectName("label_3")
+        self.gridLayout_2.addWidget(self.label_3, 1, 0, 1, 1)
+        self.label_7 = QtWidgets.QLabel(InfoForm)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+                                           QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(3)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.label_7.sizePolicy().hasHeightForWidth())
+        self.label_7.setSizePolicy(sizePolicy)
+        self.label_7.setStyleSheet("QLabel {\n"
+                                   "    font-size: 14px;\n"
+                                   "    font-weight: bold;\n"
+                                   "}")
+        self.label_7.setObjectName("label_7")
+        self.gridLayout_2.addWidget(self.label_7, 1, 1, 1, 1)
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setContentsMargins(10, 0, 0, 10)
+        self.gridLayout.setObjectName("gridLayout")
+        self.fav_on_lbl = QtWidgets.QLabel(InfoForm)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.fav_on_lbl.setFont(font)
+        self.fav_on_lbl.setStyleSheet("QLabel {\n"
+                                      "    margin-left: 6px;\n"
+                                      "    font-size: 14pt;\n"
+                                      "}")
+        self.fav_on_lbl.setObjectName("fav_on_lbl")
+        self.gridLayout.addWidget(self.fav_on_lbl, 2, 1, 1, 1)
+        self.fav_btn = QtWidgets.QToolButton(InfoForm)
+        self.fav_btn.setText("")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/icons/fav-off-action.png"),
+                       QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap(":/icons/fav-on-action.png"),
+                       QtGui.QIcon.Active, QtGui.QIcon.On)
+        icon.addPixmap(QtGui.QPixmap(":/icons/fav-on-action.png"),
+                       QtGui.QIcon.Selected, QtGui.QIcon.On)
+        self.fav_btn.setIcon(icon)
+        self.fav_btn.setIconSize(QtCore.QSize(32, 32))
+        self.fav_btn.setCheckable(True)
+        self.fav_btn.setAutoRaise(True)
+        self.fav_btn.setObjectName("fav_btn")
+        self.gridLayout.addWidget(self.fav_btn, 2, 0, 1, 1)
+        self.label_5 = QtWidgets.QLabel(InfoForm)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.label_5.setFont(font)
+        self.label_5.setStyleSheet("QLabel {\n"
+                                   "    margin-left: 6px;\n"
+                                   "    font-size: 14pt;\n"
+                                   "}")
+        self.label_5.setObjectName("label_5")
+        self.gridLayout.addWidget(self.label_5, 1, 1, 1, 1)
+        self.open_in_terminal_btn = QtWidgets.QToolButton(InfoForm)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap(":/icons/console.png"),
+                        QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.open_in_terminal_btn.setIcon(icon1)
+        self.open_in_terminal_btn.setIconSize(QtCore.QSize(32, 32))
+        self.open_in_terminal_btn.setCheckable(False)
+        self.open_in_terminal_btn.setAutoRaise(True)
+        self.open_in_terminal_btn.setObjectName("open_in_terminal_btn")
+        self.gridLayout.addWidget(self.open_in_terminal_btn, 1, 0, 1, 1)
+        self.label_4 = QtWidgets.QLabel(InfoForm)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.label_4.setFont(font)
+        self.label_4.setStyleSheet("QLabel {\n"
+                                   "    margin-left: 6px;\n"
+                                   "    font-size: 14pt;\n"
+                                   "}")
+        self.label_4.setObjectName("label_4")
+        self.gridLayout.addWidget(self.label_4, 0, 1, 1, 1)
+        self.open_btn = QtWidgets.QToolButton(InfoForm)
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap(":/icons/run.png"), QtGui.QIcon.Normal,
+                        QtGui.QIcon.Off)
+        self.open_btn.setIcon(icon2)
+        self.open_btn.setIconSize(QtCore.QSize(32, 32))
+        self.open_btn.setAutoRaise(True)
+        self.open_btn.setObjectName("open_btn")
+        self.gridLayout.addWidget(self.open_btn, 0, 0, 1, 1)
+        self.gridLayout_2.addLayout(self.gridLayout, 2, 0, 1, 1)
         self.widget = QtWidgets.QWidget(InfoForm)
         self.widget.setStyleSheet("QWidget {\n"
                                   "    background-color: rgb(4, 177, 59);\n"
@@ -56,107 +153,45 @@ class Ui_InfoForm(object):
                                           "}")
         self.app_main_group.setObjectName("app_main_group")
         self.horizontalLayout.addWidget(self.app_main_group)
+        self.toolButton = QtWidgets.QToolButton(self.widget)
+        icon3 = QtGui.QIcon()
+        icon3.addPixmap(QtGui.QPixmap(":/icons/close.png"), QtGui.QIcon.Normal,
+                        QtGui.QIcon.Off)
+        self.toolButton.setIcon(icon3)
+        self.toolButton.setIconSize(QtCore.QSize(32, 32))
+        self.toolButton.setAutoRaise(True)
+        self.toolButton.setObjectName("toolButton")
+        self.horizontalLayout.addWidget(self.toolButton)
         self.gridLayout_2.addWidget(self.widget, 0, 0, 1, 2)
-        self.label_3 = QtWidgets.QLabel(InfoForm)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
-                                           QtWidgets.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(1)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.label_3.sizePolicy().hasHeightForWidth())
-        self.label_3.setSizePolicy(sizePolicy)
-        self.label_3.setObjectName("label_3")
-        self.gridLayout_2.addWidget(self.label_3, 1, 0, 1, 1)
-        self.label_7 = QtWidgets.QLabel(InfoForm)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
-                                           QtWidgets.QSizePolicy.Preferred)
+        self.desc_plainTextEdit = QtWidgets.QPlainTextEdit(InfoForm)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding,
+                                           QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(3)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(
-            self.label_7.sizePolicy().hasHeightForWidth())
-        self.label_7.setSizePolicy(sizePolicy)
-        self.label_7.setObjectName("label_7")
-        self.gridLayout_2.addWidget(self.label_7, 1, 1, 1, 1)
-        self.gridLayout = QtWidgets.QGridLayout()
-        self.gridLayout.setObjectName("gridLayout")
-        self.open_btn = QtWidgets.QToolButton(InfoForm)
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(":/icons/run.png"), QtGui.QIcon.Normal,
-                       QtGui.QIcon.Off)
-        self.open_btn.setIcon(icon)
-        self.open_btn.setIconSize(QtCore.QSize(32, 32))
-        self.open_btn.setAutoRaise(True)
-        self.open_btn.setObjectName("open_btn")
-        self.gridLayout.addWidget(self.open_btn, 0, 0, 1, 1)
-        self.label_4 = QtWidgets.QLabel(InfoForm)
-        font = QtGui.QFont()
-        font.setPointSize(14)
-        self.label_4.setFont(font)
-        self.label_4.setObjectName("label_4")
-        self.gridLayout.addWidget(self.label_4, 0, 1, 1, 1)
-        self.open_in_terminal_btn = QtWidgets.QToolButton(InfoForm)
-        icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap(":/icons/console.png"),
-                        QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.open_in_terminal_btn.setIcon(icon1)
-        self.open_in_terminal_btn.setIconSize(QtCore.QSize(32, 32))
-        self.open_in_terminal_btn.setCheckable(False)
-        self.open_in_terminal_btn.setAutoRaise(True)
-        self.open_in_terminal_btn.setObjectName("open_in_terminal_btn")
-        self.gridLayout.addWidget(self.open_in_terminal_btn, 1, 0, 1, 1)
-        self.label_5 = QtWidgets.QLabel(InfoForm)
-        font = QtGui.QFont()
-        font.setPointSize(14)
-        self.label_5.setFont(font)
-        self.label_5.setObjectName("label_5")
-        self.gridLayout.addWidget(self.label_5, 1, 1, 1, 1)
-        self.fav_btn = QtWidgets.QToolButton(InfoForm)
-        self.fav_btn.setText("")
-        icon2 = QtGui.QIcon()
-        icon2.addPixmap(QtGui.QPixmap(":/icons/fav-off-action.png"),
-                        QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        icon2.addPixmap(QtGui.QPixmap(":/icons/fav-on-action.png"),
-                        QtGui.QIcon.Active, QtGui.QIcon.On)
-        icon2.addPixmap(QtGui.QPixmap(":/icons/fav-on-action.png"),
-                        QtGui.QIcon.Selected, QtGui.QIcon.On)
-        self.fav_btn.setIcon(icon2)
-        self.fav_btn.setIconSize(QtCore.QSize(32, 32))
-        self.fav_btn.setCheckable(True)
-        self.fav_btn.setAutoRaise(True)
-        self.fav_btn.setObjectName("fav_btn")
-        self.gridLayout.addWidget(self.fav_btn, 2, 0, 1, 1)
-        self.label_6 = QtWidgets.QLabel(InfoForm)
-        font = QtGui.QFont()
-        font.setPointSize(14)
-        self.label_6.setFont(font)
-        self.label_6.setObjectName("label_6")
-        self.gridLayout.addWidget(self.label_6, 2, 1, 1, 1)
-        self.gridLayout_2.addLayout(self.gridLayout, 2, 0, 1, 1)
-        self.desc_plainTextEdit = QtWidgets.QPlainTextEdit(InfoForm)
+            self.desc_plainTextEdit.sizePolicy().hasHeightForWidth())
+        self.desc_plainTextEdit.setSizePolicy(sizePolicy)
         self.desc_plainTextEdit.setStyleSheet("QPlainTextEdit {\n"
-                                              "    font-size: 12pt;\n"
+                                              "    font-size: 13pt;\n"
+                                              "    border: none;\n"
+                                              "    margin-bottom: 6px;\n"
+                                              "    margin-right: 6px;\n"
                                               "}")
+        self.desc_plainTextEdit.setReadOnly(True)
         self.desc_plainTextEdit.setObjectName("desc_plainTextEdit")
         self.gridLayout_2.addWidget(self.desc_plainTextEdit, 2, 1, 1, 1)
 
         self.retranslateUi(InfoForm)
+        self.toolButton.clicked.connect(InfoForm.on_close)
         self.fav_btn.toggled['bool'].connect(InfoForm.on_toggle_fav)
-        self.open_btn.clicked.connect(InfoForm.on_run_app)
         self.open_in_terminal_btn.clicked.connect(
             InfoForm.on_run_app_in_terminal)
+        self.open_btn.clicked.connect(InfoForm.on_run_app)
         QtCore.QMetaObject.connectSlotsByName(InfoForm)
 
     def retranslateUi(self, InfoForm):
         _translate = QtCore.QCoreApplication.translate
         InfoForm.setWindowTitle(_translate("InfoForm", "Form"))
-        self.app_name.setText(
-            _translate("InfoForm",
-                       "<html><head/><body><p>App Name</p></body></html>"))
-        self.app_main_group.setText(
-            _translate(
-                "InfoForm",
-                "<html><head/><body><p><span style=\" font-size:16pt; color:#ffffff;\">App Main Group Name</span></p></body></html>"
-            ))
         self.label_3.setText(
             _translate(
                 "InfoForm",
@@ -167,11 +202,20 @@ class Ui_InfoForm(object):
                 "InfoForm",
                 "<html><head/><body><p><span style=\" font-size:14pt; font-weight:600;\">Descriptions:</span></p></body></html>"
             ))
-        self.open_btn.setText(_translate("InfoForm", "..."))
-        self.label_4.setText(_translate("InfoForm", "Open"))
-        self.open_in_terminal_btn.setText(_translate("InfoForm", "..."))
+        self.fav_on_lbl.setText(_translate("InfoForm", "Add to Favorites"))
         self.label_5.setText(_translate("InfoForm", "Open in Terminal"))
-        self.label_6.setText(_translate("InfoForm", "Add to Favorites"))
+        self.open_in_terminal_btn.setText(_translate("InfoForm", "..."))
+        self.label_4.setText(_translate("InfoForm", "Open"))
+        self.open_btn.setText(_translate("InfoForm", "..."))
+        self.app_name.setText(
+            _translate("InfoForm",
+                       "<html><head/><body><p>App Name</p></body></html>"))
+        self.app_main_group.setText(
+            _translate(
+                "InfoForm",
+                "<html><head/><body><p><span style=\" font-size:16pt; color:#ffffff;\">App Main Group Name</span></p></body></html>"
+            ))
+        self.toolButton.setText(_translate("InfoForm", "..."))
 
 
 from . import resources_rc
