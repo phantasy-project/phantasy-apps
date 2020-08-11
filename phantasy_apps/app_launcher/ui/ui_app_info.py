9f49bd1a71168b82f6b84d524a4e5fb7d7df1356
Tong Zhang <zhangt@frib.msu.edu>
2020-08-11 16:55:41 -0400

    UI/AL: Update UI and resources.

--- phantasy_apps/app_launcher/ui/ui_app_info.py
+++ phantasy_apps/app_launcher/ui/ui_app_info.py
@@ -12,30 +12,13 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_InfoForm(object):
     def setupUi(self, InfoForm):
         InfoForm.setObjectName("InfoForm")
-        InfoForm.resize(979, 258)
+        InfoForm.resize(1047, 282)
         InfoForm.setStyleSheet("QWidget#InfoForm {\n"
                                "    border: 2px solid #04B13B;\n"
                                "}")
         self.gridLayout_2 = QtWidgets.QGridLayout(InfoForm)
-        self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout_2.setContentsMargins(0, 0, 0, 6)
         self.gridLayout_2.setObjectName("gridLayout_2")
-        self.desc_plainTextEdit = QtWidgets.QPlainTextEdit(InfoForm)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding,
-                                           QtWidgets.QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(3)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.desc_plainTextEdit.sizePolicy().hasHeightForWidth())
-        self.desc_plainTextEdit.setSizePolicy(sizePolicy)
-        self.desc_plainTextEdit.setStyleSheet("QPlainTextEdit {\n"
-                                              "    font-size: 13pt;\n"
-                                              "    border: none;\n"
-                                              "    margin-bottom: 6px;\n"
-                                              "    margin-right: 6px;\n"
-                                              "}")
-        self.desc_plainTextEdit.setReadOnly(True)
-        self.desc_plainTextEdit.setObjectName("desc_plainTextEdit")
-        self.gridLayout_2.addWidget(self.desc_plainTextEdit, 2, 1, 1, 1)
         self.headline_widget = QtWidgets.QWidget(InfoForm)
         self.headline_widget.setStyleSheet(
             "QWidget {\n"
@@ -94,34 +77,40 @@ class Ui_InfoForm(object):
         self.close_btn.setObjectName("close_btn")
         self.horizontalLayout.addWidget(self.close_btn)
         self.gridLayout_2.addWidget(self.headline_widget, 0, 0, 1, 2)
-        self.label_3 = QtWidgets.QLabel(InfoForm)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.contact_lbl = QtWidgets.QLabel(InfoForm)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
                                            QtWidgets.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(
-            self.label_3.sizePolicy().hasHeightForWidth())
-        self.label_3.setSizePolicy(sizePolicy)
-        self.label_3.setStyleSheet("QLabel {\n"
-                                   "    margin-left: 10px;\n"
-                                   "    font-size: 14px;\n"
-                                   "}")
-        self.label_3.setObjectName("label_3")
-        self.gridLayout_2.addWidget(self.label_3, 1, 0, 1, 1)
-        self.label_7 = QtWidgets.QLabel(InfoForm)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
-                                           QtWidgets.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(3)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.label_7.sizePolicy().hasHeightForWidth())
-        self.label_7.setSizePolicy(sizePolicy)
-        self.label_7.setStyleSheet("QLabel {\n"
-                                   "    font-size: 14px;\n"
-                                   "    font-weight: bold;\n"
-                                   "}")
-        self.label_7.setObjectName("label_7")
-        self.gridLayout_2.addWidget(self.label_7, 1, 1, 1, 1)
+            self.contact_lbl.sizePolicy().hasHeightForWidth())
+        self.contact_lbl.setSizePolicy(sizePolicy)
+        self.contact_lbl.setStyleSheet("QLabel {\n"
+                                       "    margin-left: 0px;\n"
+                                       "    font-size: 12px;\n"
+                                       "}")
+        self.contact_lbl.setObjectName("contact_lbl")
+        self.horizontalLayout_2.addWidget(self.contact_lbl)
+        self.contact_name_lbl = QtWidgets.QLabel(InfoForm)
+        self.contact_name_lbl.setObjectName("contact_name_lbl")
+        self.horizontalLayout_2.addWidget(self.contact_name_lbl)
+        self.contact_phone_lbl = QtWidgets.QLabel(InfoForm)
+        self.contact_phone_lbl.setAlignment(QtCore.Qt.AlignCenter)
+        self.contact_phone_lbl.setObjectName("contact_phone_lbl")
+        self.horizontalLayout_2.addWidget(self.contact_phone_lbl)
+        self.contact_email_lbl = QtWidgets.QLabel(InfoForm)
+        self.contact_email_lbl.setStyleSheet("QLabel {\n"
+                                             "    margin-right: 6px;\n"
+                                             "}")
+        self.contact_email_lbl.setAlignment(QtCore.Qt.AlignRight
+                                            | QtCore.Qt.AlignTrailing
+                                            | QtCore.Qt.AlignVCenter)
+        self.contact_email_lbl.setOpenExternalLinks(True)
+        self.contact_email_lbl.setObjectName("contact_email_lbl")
+        self.horizontalLayout_2.addWidget(self.contact_email_lbl)
+        self.gridLayout_2.addLayout(self.horizontalLayout_2, 3, 1, 1, 1)
         self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setContentsMargins(10, 0, 0, 10)
         self.gridLayout.setObjectName("gridLayout")
@@ -188,6 +177,58 @@ class Ui_InfoForm(object):
         self.open_btn.setObjectName("open_btn")
         self.gridLayout.addWidget(self.open_btn, 0, 0, 1, 1)
         self.gridLayout_2.addLayout(self.gridLayout, 2, 0, 1, 1)
+        self.label_7 = QtWidgets.QLabel(InfoForm)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+                                           QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(3)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.label_7.sizePolicy().hasHeightForWidth())
+        self.label_7.setSizePolicy(sizePolicy)
+        self.label_7.setStyleSheet("QLabel {\n"
+                                   "    font-size: 14px;\n"
+                                   "    font-weight: bold;\n"
+                                   "}")
+        self.label_7.setObjectName("label_7")
+        self.gridLayout_2.addWidget(self.label_7, 1, 1, 1, 1)
+        self.label_3 = QtWidgets.QLabel(InfoForm)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+                                           QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.label_3.sizePolicy().hasHeightForWidth())
+        self.label_3.setSizePolicy(sizePolicy)
+        self.label_3.setStyleSheet("QLabel {\n"
+                                   "    margin-left: 10px;\n"
+                                   "    font-size: 14px;\n"
+                                   "}")
+        self.label_3.setObjectName("label_3")
+        self.gridLayout_2.addWidget(self.label_3, 1, 0, 1, 1)
+        self.desc_plainTextEdit = QtWidgets.QPlainTextEdit(InfoForm)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding,
+                                           QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(3)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.desc_plainTextEdit.sizePolicy().hasHeightForWidth())
+        self.desc_plainTextEdit.setSizePolicy(sizePolicy)
+        self.desc_plainTextEdit.setStyleSheet("QPlainTextEdit {\n"
+                                              "    font-size: 13pt;\n"
+                                              "    border: none;\n"
+                                              "    margin-bottom: 6px;\n"
+                                              "    margin-right: 6px;\n"
+                                              "}")
+        self.desc_plainTextEdit.setReadOnly(True)
+        self.desc_plainTextEdit.setObjectName("desc_plainTextEdit")
+        self.gridLayout_2.addWidget(self.desc_plainTextEdit, 2, 1, 1, 1)
+        self.label_3.raise_()
+        self.label_7.raise_()
+        self.headline_widget.raise_()
+        self.desc_plainTextEdit.raise_()
+        self.contact_phone_lbl.raise_()
+        self.contact_email_lbl.raise_()
+        self.contact_name_lbl.raise_()
 
         self.retranslateUi(InfoForm)
         self.close_btn.clicked.connect(InfoForm.on_close)
@@ -210,21 +251,29 @@ class Ui_InfoForm(object):
             ))
         self.helpdoc_btn.setText(_translate("InfoForm", "help"))
         self.close_btn.setText(_translate("InfoForm", "..."))
-        self.label_3.setText(
+        self.contact_lbl.setText(
             _translate(
                 "InfoForm",
-                "<html><head/><body><p><span style=\" font-size:14pt;\">Actions:</span></p></body></html>"
+                "<html><head/><body><p><span style=\" font-size:14pt;\">Contact:</span></p></body></html>"
             ))
-        self.label_7.setText(
-            _translate(
-                "InfoForm",
-                "<html><head/><body><p><span style=\" font-size:14pt; font-weight:600;\">Descriptions:</span></p></body></html>"
-            ))
+        self.contact_name_lbl.setText(_translate("InfoForm", "name"))
+        self.contact_phone_lbl.setText(_translate("InfoForm", "phone"))
+        self.contact_email_lbl.setText(_translate("InfoForm", "email"))
         self.fav_on_lbl.setText(_translate("InfoForm", "Add to Favorites"))
         self.label_5.setText(_translate("InfoForm", "Open in Terminal"))
         self.open_in_terminal_btn.setText(_translate("InfoForm", "..."))
         self.label_4.setText(_translate("InfoForm", "Open"))
         self.open_btn.setText(_translate("InfoForm", "..."))
+        self.label_7.setText(
+            _translate(
+                "InfoForm",
+                "<html><head/><body><p><span style=\" font-size:14pt; font-weight:600;\">Descriptions:</span></p></body></html>"
+            ))
+        self.label_3.setText(
+            _translate(
+                "InfoForm",
+                "<html><head/><body><p><span style=\" font-size:14pt;\">Actions:</span></p></body></html>"
+            ))
 
 
 from . import resources_rc
