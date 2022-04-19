4e1787d82b86df1abd533967d2d9e64ccf2f568c
Tong Zhang <zhangt@frib.msu.edu>
2022-04-19 15:09:12 -0400

    UI/CV: Update UI.

--- phantasy_apps/correlation_visualizer/ui/ui_plot_all.py
+++ phantasy_apps/correlation_visualizer/ui/ui_plot_all.py
@@ -12,14 +12,24 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(1258, 965)
-        self.gridLayout = QtWidgets.QGridLayout(Form)
-        self.gridLayout.setObjectName("gridLayout")
-        self.matplotliberrorbarWidget = MatplotlibErrorbarWidget(Form)
+        Form.resize(1356, 851)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.splitter = QtWidgets.QSplitter(Form)
+        self.splitter.setOrientation(QtCore.Qt.Horizontal)
+        self.splitter.setObjectName("splitter")
+        self.matplotliberrorbarWidget = MatplotlibErrorbarWidget(self.splitter)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding,
+                                           QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.matplotliberrorbarWidget.sizePolicy().hasHeightForWidth())
+        self.matplotliberrorbarWidget.setSizePolicy(sizePolicy)
         self.matplotliberrorbarWidget.setFigureAutoScale(True)
         font = QtGui.QFont()
         font.setFamily("sans-serif")
-        font.setPointSize(14)
+        font.setPointSize(15)
         font.setBold(False)
         font.setItalic(False)
         font.setWeight(50)
@@ -32,7 +42,7 @@ class Ui_Form(object):
         font.setWeight(50)
         self.matplotliberrorbarWidget.setFigureTitleFont(font)
         self.matplotliberrorbarWidget.setFigureGridToggle(True)
-        self.matplotliberrorbarWidget.setProperty("figureLegendToggle", True)
+        self.matplotliberrorbarWidget.setProperty("figureLegendToggle", False)
         font = QtGui.QFont()
         font.setFamily("sans-serif")
         font.setPointSize(14)
@@ -41,7 +51,35 @@ class Ui_Form(object):
         font.setWeight(50)
         self.matplotliberrorbarWidget.setFigureXYticksFont(font)
         self.matplotliberrorbarWidget.setObjectName("matplotliberrorbarWidget")
-        self.gridLayout.addWidget(self.matplotliberrorbarWidget, 0, 0, 1, 1)
+        self.groupBox = QtWidgets.QGroupBox(self.splitter)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum,
+                                           QtWidgets.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.groupBox.sizePolicy().hasHeightForWidth())
+        self.groupBox.setSizePolicy(sizePolicy)
+        self.groupBox.setObjectName("groupBox")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.groupBox)
+        self.verticalLayout.setContentsMargins(6, 6, 6, 6)
+        self.verticalLayout.setSpacing(4)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.option_area = QtWidgets.QScrollArea(self.groupBox)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+                                           QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.option_area.sizePolicy().hasHeightForWidth())
+        self.option_area.setSizePolicy(sizePolicy)
+        self.option_area.setWidgetResizable(True)
+        self.option_area.setObjectName("option_area")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 891, 777))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.option_area.setWidget(self.scrollAreaWidgetContents)
+        self.verticalLayout.addWidget(self.option_area)
+        self.verticalLayout_2.addWidget(self.splitter)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -49,6 +87,7 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
+        self.groupBox.setTitle(_translate("Form", "Curves"))
 
 
 from mpl4qt.widgets.mplerrorbarwidget import MatplotlibErrorbarWidget
