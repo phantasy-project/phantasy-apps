d85a11fab7094c6c8fdce56359febeedde7db623
Tong Zhang <zhangt@frib.msu.edu>
2022-04-19 10:31:49 -0400

    CV/UI: Initial widget for plotting all curves.

---
+++ phantasy_apps/correlation_visualizer/ui/ui_plot_all.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui_plot_all.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(1258, 965)
+        self.gridLayout = QtWidgets.QGridLayout(Form)
+        self.gridLayout.setObjectName("gridLayout")
+        self.matplotliberrorbarWidget = MatplotlibErrorbarWidget(Form)
+        self.matplotliberrorbarWidget.setObjectName("matplotliberrorbarWidget")
+        self.gridLayout.addWidget(self.matplotliberrorbarWidget, 0, 0, 1, 1)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+
+
+from mpl4qt.widgets.mplerrorbarwidget import MatplotlibErrorbarWidget
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
