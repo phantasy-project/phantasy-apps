ea6990930ddf299281266c770b829bf1812f7489
Tong Zhang <zhangt@frib.msu.edu>
2022-06-22 16:53:44 -0400

    MNT/SM: Bump version to 10.0.

--- phantasy_apps/settings_manager/__init__.py
+++ phantasy_apps/settings_manager/__init__.py
@@ -7,11 +7,11 @@ from phantasy_ui import QApp as QApplication
 from .app import SettingsManagerWindow
 
 __authors__ = "Tong Zhang"
-__copyright__ = "(c) 2019-2021, Facility for Rare Isotope beams," \
+__copyright__ = "(c) 2019-2022, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
 __title__ = "Settings Manager: Manage Physics Configurations of Accelerator System"
-__version__ = '9.8'
+__version__ = '10.0'
 
 
 def run(cli=False):
