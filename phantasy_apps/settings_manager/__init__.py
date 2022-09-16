b8b517f9c92a480a629a421f042a8690da410d9b
Tong Zhang <zhangt@frib.msu.edu>
2022-09-16 11:56:22 -0400

    MNT/SM: Bump version to 10.3-2.

--- phantasy_apps/settings_manager/__init__.py
+++ phantasy_apps/settings_manager/__init__.py
@@ -11,7 +11,7 @@ __copyright__ = "(c) 2019-2022, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
 __title__ = "Settings Manager: Manage Physics Configurations of Accelerator System"
-__version__ = '10.3-1'
+__version__ = '10.3-2'
 
 
 def run(cli=False):
