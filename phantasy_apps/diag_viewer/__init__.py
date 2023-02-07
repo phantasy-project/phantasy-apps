256f349c4f9e60709889a6317bcd3f9bac321501
Tong Zhang <zhangt@frib.msu.edu>
2023-02-07 09:53:35 -0500

    MNT/DV: Bump version to 3.1.

--- phantasy_apps/diag_viewer/__init__.py
+++ phantasy_apps/diag_viewer/__init__.py
@@ -13,11 +13,11 @@ from phantasy_ui import set_mplstyle
 from .app import DeviceViewerWindow
 
 __authors__ = "Tong Zhang"
-__copyright__ = "(c) 2019, Facility for Rare Isotope beams," \
+__copyright__ = "(c) 2019-2023, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
 __title__ = "Device Viewer: Visualize Device Readings and Settings"
-__version__ = '3.0'
+__version__ = '3.1'
 
 
 def run(cli=False):
