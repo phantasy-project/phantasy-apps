e0176818fa9ad26687aa71187a24a30c147bb2f4
Tong Zhang <zhangt@frib.msu.edu>
2023-05-24 16:54:43 -0400

    REF/TM: Tabify widgets.

--- phantasy_apps/threshold_manager/app.py
+++ phantasy_apps/threshold_manager/app.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 import toml
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QTimer
 from phantasy_ui import BaseAppForm
+from phantasy_ui.widgets import DockWidget
 from phantasy_apps.threshold_manager._widget import MPSDiagWidget
 from phantasy_apps.threshold_manager._widget import SnapshotWidget
 from phantasy_apps.threshold_manager.ui.ui_app import Ui_MainWindow
 
 OUT_DATA_DIR = "/tmp"
 
+_CDIR = os.path.dirname(__file__)
 
 def read_config(configpath: str):
     _c = toml.load(configpath)
@@ -36,23 +41,48 @@ class MPSThresholdManagerWindow(BaseAppForm, Ui_MainWindow):
         # UI
         self.setupUi(self)
         self.postInitUi()
-        # resizing
-        self.resize(1920, 1440)
 
         self._post_init()
+        # resizing
+        self.resize(1920, 1440)
 
     def _post_init(self):
         self.nd_widget = MPSDiagWidget("ND", OUT_DATA_DIR)
         self.ic_widget = MPSDiagWidget("IC", OUT_DATA_DIR)
         self.hmr_widget = MPSDiagWidget("HMR", OUT_DATA_DIR)
 
-        self.nb_hbox.addWidget(self.nd_widget)
-        self.ic_hbox.addWidget(self.ic_widget)
-        self.hmr_hbox.addWidget(self.hmr_widget)
+        self.nd_dock = DockWidget(self)
+        self.nd_dock.setWindowTitle("Neutron Detectors")
+        self.ic_dock = DockWidget(self)
+        self.ic_dock.setWindowTitle("Ionization Chambers")
+        self.hmr_dock = DockWidget(self)
+        self.hmr_dock.setWindowTitle("Halo Monitor Rings")
 
+        self.nd_dock.setWidget(self.nd_widget)
+        self.ic_dock.setWidget(self.ic_widget)
+        self.hmr_dock.setWidget(self.hmr_widget)
+
+        self.addDockWidget(Qt.LeftDockWidgetArea, self.nd_dock)
+        self.addDockWidget(Qt.RightDockWidgetArea, self.ic_dock)
+        self.tabifyDockWidget(self.nd_dock, self.ic_dock)
+        self.tabifyDockWidget(self.ic_dock, self.hmr_dock)
+        QTimer.singleShot(50, lambda:self.nd_dock.raise_())
+
+        # snapshot dock
         self.snp_widget = SnapshotWidget("ND")
-        self.snp_hbox.addWidget(self.snp_widget)
+        self.snp_dock = DockWidget(self)
+        self.snp_dock.setWindowTitle("Snapshots")
+        self.snp_dock.setWidget(self.snp_widget)
+        self.addDockWidget(Qt.TopDockWidgetArea, self.snp_dock)
+
 
         # test:
-        test_db_path = "/home/tong/Dropbox/phantasy-project/phantasy-apps/phantasy_apps/threshold_manager/tests/mps_model/test.db"
+        test_db_path = os.path.join(_CDIR, "tests/mps_model/test.db")
         self.snp_widget.db_path_lineEdit.setText(test_db_path)
+
+
+    def resizeEvent(self, e):
+        # resize dock widget
+        self.resizeDocks([self.nd_dock, self.ic_dock, self.hmr_dock],
+                [self.width(), self.width(), self.width()], Qt.Horizontal)
+        BaseAppForm.resizeEvent(self, e)
