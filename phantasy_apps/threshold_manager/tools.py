c01c4732b7b433849732a0d26cee9778473ee842
Tong Zhang <zhangt@frib.msu.edu>
2023-06-08 08:45:01 -0400

    REF/TM: Add tools.
    - missed module.

---
+++ phantasy_apps/threshold_manager/tools.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import time
+from getpass import getuser
+import sqlite3
+from phantasy_ui.widgets import BeamSpeciesDisplayWidget
+from phantasy_apps.threshold_manager.data import (SnapshotData, ISRC_INDEX_MAP)
+from phantasy_apps.threshold_manager.db.utils import insert_update_data
+
+from ._model import _get_dataframe
+
+TABLE_NAME = "mps_threshold"
+
+
+def take_snapshot(dtypes: list[str],
+                  note: str,
+                  tags: list,
+                  conn: sqlite3.Connection,
+                  meta_isrc_name: str = 'Live'):
+    """Take a snasphot based on *dtype* for the MPS threshold data.
+
+    Parameters
+    ----------
+    dtypes : list
+        A list of device types, a device type could be `ND`, `IC` or `HMR`.
+    note : str
+        A string of note.
+    tags : list
+        A list of strings as tags.
+    conn : sqlite3.Connection
+        Database connection object.
+    meta_isrc_name : str
+        Name of the ion source, could be `Live`, `Artemis` or `HP-ECR`.
+    """
+    ts = time.time()
+    user = getuser()
+    isrc_name, ion_name, ion_mass, ion_number, ion_charge, ion_charge_state, \
+        beam_power, beam_energy, beam_bound, beam_dest = \
+            BeamSpeciesDisplayWidget.get_species_meta_full(ISRC_INDEX_MAP[meta_isrc_name])
+    data_dict = {_dtype: _get_dataframe(_dtype) for _dtype in dtypes}
+    _row_data = (ts, user, isrc_name, ion_name, ion_number, ion_mass, ion_charge,
+                 ion_charge_state, beam_power, beam_energy, beam_bound, beam_dest,
+                 ','.join(tags), note, data_dict)
+    snp_data = SnapshotData(_row_data)
+    insert_update_data(conn, snp_data, TABLE_NAME)
+    print(f"Saved data {snp_data} to the database.")
