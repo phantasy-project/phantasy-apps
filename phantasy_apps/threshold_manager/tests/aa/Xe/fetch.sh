5b8ad4fd21086fbf1a16da3078fa08ab721e4490
Tong Zhang <zhangt@frib.msu.edu>
2023-07-18 13:55:33 -0400

    MNT/TM: Add script for fetching data for Xe.

---
+++ phantasy_apps/threshold_manager/tests/aa/Xe/fetch.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+#
+# Fetch archived data for accumulated dose calculation.
+#
+
+FROM_TS=2023-04-11T18:04:00.00-04:00 
+TO_TS=2023-04-11T22:55:00.00-04:00
+
+# ND
+echo "Fetching data for ND from ${FROM_TS} to ${TO_TS}"
+pyarchappl-get --pv-file ../nd_pv.txt -vv \
+    --from ${FROM_TS} --to ${TO_TS} \
+    --output data_nd_1s.csv \
+    --resample 1S
+
+sleep 2
+
+# IC
+echo "Fetching data for IC from ${FROM_TS} to ${TO_TS}"
+pyarchappl-get --pv-file ../ic_pv.txt -vv \
+    --from ${FROM_TS} --to ${TO_TS} \
+    --output data_ic_1s.csv \
+    --resample 1S
+
+sleep 2
+
+# HMR
+echo "Fetching data for HMR from ${FROM_TS} to ${TO_TS}"
+pyarchappl-get --pv-file ../hmr_pv.txt -vv \
+    --from ${FROM_TS} --to ${TO_TS} \
+    --output data_hmr_1s.csv \
+    --resample 1S
