a4dc5629911a3d57f6bbd379786b01c231de707d
Tong Zhang <zhangt@frib.msu.edu>
2023-08-24 14:10:16 -0400

    REF/SM: Update the database operation functions.
    - insert/update/delete

--- phantasy_apps/settings_manager/db_utils.py
+++ phantasy_apps/settings_manager/db_utils.py
@@ -12,13 +12,14 @@ def insert_update_data(conn, snp_data):
 
 
 def insert_data(conn, snp_data):
-    data_tuple = snp_data.timestamp, snp_data.datetime, snp_data.name, \
-         snp_data.note, \
-         snp_data.user, snp_data.ion_name, \
-         int(snp_data.ion_number), int(snp_data.ion_mass), \
-         int(snp_data.ion_charge), snp_data.machine, \
+    data_tuple = snp_data.timestamp, snp_data.datetime, \
+         snp_data.note, snp_data.user, snp_data.ion_name, \
+         snp_data.ion_number, snp_data.ion_mass, \
+         snp_data.ion_charge, snp_data.machine, \
          snp_data.segment, snp_data.tags_as_str(), \
-         snp_data.app, snp_data.version, 'xlsx', snp_data.to_blob()
+         snp_data.app, snp_data.version, \
+         snp_data.data_format, snp_data.to_blob(), \
+         snp_data.date, snp_data.parent
     cursor = conn.cursor()
     _insert_data(cursor, *data_tuple)
     conn.commit()
@@ -27,23 +28,23 @@ def insert_data(conn, snp_data):
 def update_data(conn, snp_data):
     cursor = conn.cursor()
     query = """ UPDATE snapshot SET
-    note = ?, tags = ? WHERE name = ? """
-    cursor.execute(query, (snp_data.note, snp_data.tags_as_str(), snp_data.name))
+    note = ?, tags = ? WHERE datetime = ? """
+    cursor.execute(query, (snp_data.note, snp_data.tags_as_str(), snp_data.datetime))
     conn.commit()
     cursor.close()
 
 
 def get_data(conn, snp_data):
     cursor = conn.cursor()
-    name = snp_data.name
-    r = cursor.execute(f" SELECT * FROM snapshot WHERE name = '{name}' ")
+    datetime = snp_data.name
+    r = cursor.execute(f" SELECT * FROM snapshot WHERE datetime = '{datetime}' ")
     data = r.fetchall()
     cursor.close()
     return data
 
 
 def _insert_data(cursor, *data):
-    query = ''' INSERT INTO snapshot (timestamp, datetime, name, note, user, ion_name, ion_number, ion_mass, ion_charge, machine, segment, tags, app, version, data_format, data) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?); '''
+    query = ''' INSERT INTO snapshot (timestamp, datetime, note, user, ion_name, ion_number, ion_mass, ion_charge, machine, segment, tags, app, version, data_format, data, date, parent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?); '''
     try:
         cursor.execute(query, data)
     except Error as err:
@@ -54,9 +55,9 @@ def _insert_data(cursor, *data):
 
 def delete_data(conn, snp_data):
     cursor = conn.cursor()
-    name = snp_data.name
+    datetime = snp_data.datetime
     try:
-        cursor.execute(f''' DELETE FROM snapshot WHERE name = '{name}' ''')
+        cursor.execute(f''' DELETE FROM snapshot WHERE datetime = '{datetime}' ''')
     except Error as err:
         print(err)
     else:
