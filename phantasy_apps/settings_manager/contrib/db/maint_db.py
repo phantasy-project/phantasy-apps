f110cb6682a677d3fad1a418dcc9a7bd428e23af
Tong Zhang <zhangt@frib.msu.edu>
2023-09-12 14:54:33 -0400

    REF/SM: Replace 'datetime' with 'name' column for duplicated datetime rows.

--- phantasy_apps/settings_manager/contrib/db/maint_db.py
+++ phantasy_apps/settings_manager/contrib/db/maint_db.py
@@ -23,16 +23,20 @@ def process_db(input_dbpath: str, output_dbpath: str):
     con = sqlite3.connect(input_dbpath)
     df = pd.read_sql('SELECT * FROM snapshot', con)
     df['date'] = df['timestamp'].apply(lambda i: datetime.fromtimestamp(i).strftime("%Y-%m-%d %A"))
-    df.drop(columns=['name'], inplace=True)
     con.close()
 
     # drop duplicated rows on 'datetime'
-    print("Duplicated rows on 'datetime':")
-    print(df[df.datetime.duplicated(keep='first')].loc[:, ['id', 'datetime', 'note']])
-    drop_duplicates = input("Drop duplicated items? [Y]/N")
-    if drop_duplicates in ('N', 'n'):
-        return
-    df = df[~df.datetime.duplicated(keep='first')]
+    dup_idx = df[df.datetime.duplicated()].index
+    if not dup_idx.empty:
+        print("Duplicated rows on 'datetime':")
+        print(df.loc[dup_idx, ['id', 'timestamp', 'date', 'name', 'note']])
+        replace_duplicates = input("Replace 'datetime' with 'name' for the duplicated items? [Y]/N ")
+        if replace_duplicates in ('N', 'n'):
+            return
+        df.loc[dup_idx, 'datetime'] = df.loc[dup_idx, 'name']
+
+    # drop name column
+    df.drop(columns=['name'], inplace=True)
 
     parent_list = []
     n_rows = df.shape[0]
@@ -57,9 +61,9 @@ def process_db(input_dbpath: str, output_dbpath: str):
 
 
 if __name__ == "__main__":
-    # input_db = "/home/tong/Downloads/20230816T000001_SM.db"
-    # output_db = "/home/tong/Downloads/20230816T000001_SM_new.db"
-    input_db = "/home/tong/Dropbox/phantasy-project/phantasy-apps/phantasy_apps/settings_manager/testdata/settings_manager/sm1.db"
-    output_db = "/home/tong/Dropbox/phantasy-project/phantasy-apps/phantasy_apps/settings_manager/testdata/settings_manager/sm1_new.db"
+    input_db = "/home/tong/Downloads/20230816T000001_SM.db"
+    output_db = "/home/tong/Downloads/20230816T000001_SM_new.db"
+    # input_db = "/home/tong/Dropbox/phantasy-project/phantasy-apps/phantasy_apps/settings_manager/testdata/settings_manager/sm1.db"
+    # output_db = "/home/tong/Dropbox/phantasy-project/phantasy-apps/phantasy_apps/settings_manager/testdata/settings_manager/sm1_new.db"
     process_db(input_db, output_db)
     init_db(output_db)
