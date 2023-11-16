9ec770dd8c6be7826023718b0279d4876b40b822
Tong Zhang <zhangt@frib.msu.edu>
2023-11-16 13:07:13 -0500

    MNT/AS: Bump version to 4.0.

--- phantasy_apps/allison_scanner/__init__.py
+++ phantasy_apps/allison_scanner/__init__.py
@@ -13,11 +13,11 @@ from .model import Model
 from .utils import point_in_ellipse
 
 __authors__ = "Tong Zhang"
-__copyright__ = "(c) 2019-2020, Facility for Rare Isotope beams," \
+__copyright__ = "(c) 2019-2023, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
 __title__ = "Allison Scanner: Measure Transverse Emittance and Twiss Parameters"
-__version__ = '3.6'
+__version__ = '4.0'
 
 
 def run(cli=False):
